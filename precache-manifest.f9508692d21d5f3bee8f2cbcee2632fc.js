4cfb4d1e860026f497c6da18ad0b376b68811052
subhash chandar <subhashchandar.oc766@webkul.in>
2024-05-15 17:35:38 +0530

    upload react demo

---
+++ precache-manifest.f9508692d21d5f3bee8f2cbcee2632fc.js
@@ -0,0 +1,86 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "467d87ce8903157239af1f8a8e5e585a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e793180a497dac44e5ca",
+    "url": "/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "fdab7eb15151241d80f7",
+    "url": "/static/js/16.92f96523.chunk.js"
+  },
+  {
+    "revision": "a6082b8127a0981f31a709f5ad9a2cb4",
+    "url": "/static/js/16.92f96523.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1b78155a4d0bb8275adc",
+    "url": "/static/js/17.75755950.chunk.js"
+  },
+  {
+    "revision": "e793180a497dac44e5ca",
+    "url": "/static/js/main.00990d4c.chunk.js"
+  },
+  {
+    "revision": "27ae06e0ec97d8a288ec",
+    "url": "/static/js/reactPlayerDailyMotion.6aefb383.chunk.js"
+  },
+  {
+    "revision": "3922df74786aebdead66",
+    "url": "/static/js/reactPlayerFacebook.c32143bc.chunk.js"
+  },
+  {
+    "revision": "8b9b5db3e56cbe772bfe",
+    "url": "/static/js/reactPlayerFilePlayer.f88e3f7e.chunk.js"
+  },
+  {
+    "revision": "dfaf8aee09a49afcdc58",
+    "url": "/static/js/reactPlayerKaltura.a4410a37.chunk.js"
+  },
+  {
+    "revision": "d85fe322a4e6ab26163f",
+    "url": "/static/js/reactPlayerMixcloud.dfabf3e2.chunk.js"
+  },
+  {
+    "revision": "c9c680c7b5fe78e84afc",
+    "url": "/static/js/reactPlayerMux.cf0c718d.chunk.js"
+  },
+  {
+    "revision": "e8284ae790c9b7d4580a",
+    "url": "/static/js/reactPlayerPreview.2861738e.chunk.js"
+  },
+  {
+    "revision": "2d02ae45b603ba0bb61f",
+    "url": "/static/js/reactPlayerSoundCloud.523d0c0a.chunk.js"
+  },
+  {
+    "revision": "8fe4c256cfaf62cee909",
+    "url": "/static/js/reactPlayerStreamable.4b78c0d7.chunk.js"
+  },
+  {
+    "revision": "0d0a30999fa6f2059905",
+    "url": "/static/js/reactPlayerTwitch.9ac569c4.chunk.js"
+  },
+  {
+    "revision": "4d31b7ebb7580f3c37df",
+    "url": "/static/js/reactPlayerVidyard.73360940.chunk.js"
+  },
+  {
+    "revision": "97770ba4a9f06449f3c2",
+    "url": "/static/js/reactPlayerVimeo.f2a6babc.chunk.js"
+  },
+  {
+    "revision": "60751846c7fa55802be6",
+    "url": "/static/js/reactPlayerWistia.5a9b4cba.chunk.js"
+  },
+  {
+    "revision": "26999d11b5048b0bcaaa",
+    "url": "/static/js/reactPlayerYouTube.bae9ef8d.chunk.js"
+  },
+  {
+    "revision": "4b5461b7bbf915343d04",
+    "url": "/static/js/runtime-main.7d368aba.js"
+  }
+]);
